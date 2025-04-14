155fc952e45effd42db673e087e1886a1b1dcc4c
AustinD45 <duncan.austin@student.greenriver.edu>
2025-04-13 21:46:54 -0700

    On a diet

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created Austin Duncan
+
+print "Hello World!"
+
+user_name = input ("What is your name? ")
+
+print("Hello {0}".format(user_name))
